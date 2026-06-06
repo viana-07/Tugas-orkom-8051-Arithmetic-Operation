de7c2b61a5aa04333c8475d84c94703e71b3e1f3
viana-07 <delviananamira@mail.ugm.ac.id>
2026-06-06 20:11:04 +0700

    Update sub.asm

--- 16-bit/asm/sub.asm
+++ 16-bit/asm/sub.asm
@@ -1,66 +1,63 @@
-ljmp start
+	ljmp start
 
-i_ext0: ljmp j_ext0
-        nop
-        nop
-        nop
-        nop
-        nop
+i_ext0:	ljmp j_ext0
+	nop
+	nop
+	nop
+	nop
+	nop
 
-i_tim0: ljmp j_tim0
-        nop
-        nop
-        nop
-        nop
-        nop
+i_tim0:	ljmp j_tim0
+	nop
+	nop
+	nop
+	nop
+	nop
 
-i_ext1: ljmp j_ext1
-        nop
-        nop
-        nop
-        nop
-        nop
+i_ext1:	ljmp j_ext1
+	nop
+	nop
+	nop
+	nop
+	nop
 
-i_tim1: ljmp j_tim1
-        nop
-        nop
-        nop
-        nop
-        nop
+i_tim1:	ljmp j_tim1
+	nop
+	nop
+	nop
+	nop
+	nop
 
-i_siu:  ljmp j_siu
+i_siu:	ljmp j_siu
 
 start:
-        mov sp, #70h
+	mov sp, #70h
 
-        ; === 16-bit SUBTRACTION ===
-        ; 1234H - 00F2H = 1142H
-        ; Operand 1 = 12H 34H
-        ; Operand 2 = 00H F2H
-        ; Result    = 11H 42H
+	; === SUBTRACTION 16-bit ===
+	; 1234H - 00F2H = 1142H
+	; low byte  : 34H - F2H = 42H, borrow = 1
+	; high byte : 12H - 00H - borrow = 11H
 
-        clr c
+	clr c
 
-        ; Low byte
-        mov a, #34h
-        subb a, #0f2h
-        mov 42h, a
-        mov p2, a
+	mov a, #34h
+	subb a, #0f2h
+	mov 42h, a
+	mov p0, a
 
-        ; High byte
-        mov a, #12h
-        subb a, #00h
-        mov 43h, a
-        mov p3, a
+	mov a, #12h
+	subb a, #00h
+	mov 43h, a
+	mov p1, a
 
-        jmp ende
+	jmp ende
 
-j_ext0: reti
-j_tim0: reti
-j_ext1: reti
-j_tim1: reti
-j_siu:  reti
+j_ext0:	reti
+j_tim0:	reti
+j_ext1:	reti
+j_tim1:	reti
+j_siu:	reti
 
-ende:   jmp $
+ende:	jmp $
 
-end
+	end

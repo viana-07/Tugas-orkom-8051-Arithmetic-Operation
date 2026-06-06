a771022d4246af7afecf345d76b54ba64392e1ea
viana-07 <delviananamira@mail.ugm.ac.id>
2026-06-06 10:51:34 +0700

    Create add.asm

---
+++ 16-bit/add.asm
@@ -0,0 +1,66 @@
+ljmp start
+
+i_ext0: ljmp j_ext0
+        nop
+        nop
+        nop
+        nop
+        nop
+
+i_tim0: ljmp j_tim0
+        nop
+        nop
+        nop
+        nop
+        nop
+
+i_ext1: ljmp j_ext1
+        nop
+        nop
+        nop
+        nop
+        nop
+
+i_tim1: ljmp j_tim1
+        nop
+        nop
+        nop
+        nop
+        nop
+
+i_siu:  ljmp j_siu
+
+start:
+        mov sp, #70h
+
+        ; === 16-bit ADDITION ===
+        ; 1234H + 00F2H = 1326H
+        ; Operand 1 = 12H 34H
+        ; Operand 2 = 00H F2H
+        ; Result    = 13H 26H
+
+        clr c
+
+        ; Low byte
+        mov a, #34h
+        add a, #0f2h
+        mov 40h, a
+        mov p0, a
+
+        ; High byte
+        mov a, #12h
+        addc a, #00h
+        mov 41h, a
+        mov p1, a
+
+        jmp ende
+
+j_ext0: reti
+j_tim0: reti
+j_ext1: reti
+j_tim1: reti
+j_siu:  reti
+
+ende:   jmp $
+
+end
